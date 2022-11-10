367f30c2d25f87cf9410854a4f2fc2c8c8e24d1f
RhegisMarques <rhegismarques@gmail.com>
2022-11-10 01:26:08 -0300

    up

--- script.js
+++ script.js
@@ -1,48 +1,49 @@
-const modal = document.querySelector('.modal-container')
-const tbody = document.querySelector('tbody')
-const sNome = document.querySelector('#m-nome')
-const sMatricula = document.querySelector('#m-matricula')
-const sCurso = document.querySelector('#m-curso')
-const btSalvar = document.querySelector('#btSalvar')
+const modal = document.querySelector(".modal-container");
+const tbody = document.querySelector("tbody");
+const sNome = document.querySelector("#m-nome");
+const sMatricula = document.querySelector("#m-matricula");
+const sCurso = document.querySelector("#m-curso");
+const btSalvar = document.querySelector("#btSalvar");
 
-let itens
-let id
+const getItensBD = () => JSON.parse(localStorage.getItem("dbfunc")) ?? [];
+const setItensBD = () => localStorage.setItem("dbfunc", JSON.stringify(itens));
+
+let itens;
+let id;
 
 function openModal(edit = false, index = 0) {
-  modal.classList.add('active')
+  modal.classList.add("active");
 
-  modal.onclick = e => {
-    if (e.target.className.indexOf('modal-container') !== -1) {
-      modal.classList.remove('active')
+  modal.onclick = (e) => {
+    if (e.target.className.indexOf("modal-container") !== -1) {
+      modal.classList.remove("active");
     }
-  }
+  };
 
   if (edit) {
-    sNome.value = itens[index].nome
-    sMatricula.value = itens[index].matricula
-    sCurso.value = itens[index].curso
-    id = index
+    sNome.value = itens[index].nome;
+    sMatricula.value = itens[index].matricula;
+    sCurso.value = itens[index].curso;
+    id = index;
   } else {
-    sNome.value = ''
-    sMatricula.value = ''
-    sCurso.value = ''
+    sNome.value = "";
+    sMatricula.value = "";
+    sCurso.value = "";
   }
-  
 }
 
 function editItem(index) {
-
-  openModal(true, index)
+  openModal(true, index);
 }
 
 function deleteItem(index) {
-  itens.splice(index, 1)
-  setItensBD()
-  loadItens()
+  itens.splice(index, 1);
+  setItensBD();
+  loadItens();
 }
 
 function insertItem(item, index) {
-  let tr = document.createElement('tr')
+  let tr = document.createElement("tr");
 
   tr.innerHTML = `
     <td>${item.nome}</td>
@@ -54,43 +55,42 @@ function insertItem(item, index) {
     <td class="acao">
       <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
     </td>
-  `
-  tbody.appendChild(tr)
+  `;
+  tbody.appendChild(tr);
 }
 
-btSalvar.onclick = e => {
-  
-  if (sNome.value == '' || sMatricula.value == '' || sCurso.value == '') {
-    return
+btSalvar.onclick = (e) => {
+  if (sNome.value == "" || sMatricula.value == "" || sCurso.value == "") {
+    return;
   }
 
   e.preventDefault();
 
   if (id !== undefined) {
-    itens[id].nome = sNome.value
-    itens[id].matricula = sMatricula.value
-    itens[id].curso = sCurso.value
+    itens[id].nome = sNome.value;
+    itens[id].matricula = sMatricula.value;
+    itens[id].curso = sCurso.value;
   } else {
-    itens.push({'nome': sNome.value, 'matricula': sMatricula.value, 'curso': sCurso.value})
+    itens.push({
+      nome: sNome.value,
+      matricula: sMatricula.value,
+      curso: sCurso.value,
+    });
   }
 
-  setItensBD()
+  setItensBD();
 
-  modal.classList.remove('active')
-  loadItens()
-  id = undefined
-}
+  modal.classList.remove("active");
+  loadItens();
+  id = undefined;
+};
 
 function loadItens() {
-  itens = getItensBD()
-  tbody.innerHTML = ''
+  itens = getItensBD();
+  tbody.innerHTML = "";
   itens.forEach((item, index) => {
-    insertItem(item, index)
-  })
-
+    insertItem(item, index);
+  });
 }
 
-const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
-const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
-
-loadItens()
+loadItens();
